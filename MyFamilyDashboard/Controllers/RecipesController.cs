3d554a5391ef0b482d0ea3fa66204cd209ce2683
Nicklas Schmidt Vejlgaard <nisv@netcompany.com>
2019-06-20 05:50:39 +0200

    Add project files.

---
+++ MyFamilyDashboard/Controllers/RecipesController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyFamilyDashboard.Models;
+
+namespace MyFamilyDashboard.Controllers
+{
+    public class RecipesController : Controller
+    {
+        // Recipes/Random
+        public IActionResult Random()
+        {
+            var recipe = new Recipe()
+            {
+                Name = "Butter Chicken",
+                Ingredients = new List<Ingredient>()
+                {
+                    new Ingredient()
+                    {
+                        Name = "butter",
+                        Quantity = 100,
+                        Unit = IngredientUnit.Grams
+                    },
+                    new Ingredient()
+                    {
+                        Name = "Chicken",
+                        Quantity = 1000,
+                        Unit = IngredientUnit.Grams
+                    }
+                }
+            };
+            return View(recipe);
+        }
+    }
+}
