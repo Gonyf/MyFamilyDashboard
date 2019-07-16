7894b979736b23c25858d57ee6c0fb54ae23955d
Nicklas Schmidt Vejlgaard <nisv@netcompany.com>
2019-07-16 17:08:45 +0200

    Added todo list views for index and create  and added functionality for create

--- MyFamilyDashboard/Controllers/TodoController.cs
+++ MyFamilyDashboard/Controllers/TodoController.cs
@@ -3,14 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MyFamilyDashboard.Data;
 
 namespace MyFamilyDashboard.Controllers
 {
     public class TodoController : Controller
     {
+        #region protected members
+        protected ApplicationDbContext applicationDbContext;
+
+        #endregion
+
+        public TodoController(ApplicationDbContext context)
+        {
+            applicationDbContext = context;
+        }
         public IActionResult Index()
         {
+            return View(applicationDbContext.TodoLists);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Create(TodoListDataModel todoList)
+        {
+            applicationDbContext.TodoLists.Add(todoList);
+            applicationDbContext.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
